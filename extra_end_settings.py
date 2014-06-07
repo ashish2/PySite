cf1fc86f6eb4364e251a3fe8cd523ec4ec66c42e
Ashish Ojha <vickyojha2@yahoo.com>
2014-06-07 22:20:47 +0530

    Heroku testing: postgres as default db and pushing to Heroku...

--- extra_end_settings.py
+++ extra_end_settings.py
@@ -2,7 +2,8 @@
 
 ### Settings for Heroku
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+#DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config(os.environ.['DATABASE_URL'])
 
 # honor the 'x_forwarded_proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
