aeefec08009c4a0b592addf561ce3459f5b50cf3
Ashish Ojha <vickyojha2@yahoo.com>
2014-08-20 02:23:09 +0530

    Css: Added: Font Awesome

--- static/js/all.js
+++ static/js/all.js
@@ -3,13 +3,14 @@
 
 (function($){
 
-	nav_search = "#nav-search";
-	$(nav_search)
-	.focus( function(e){ $(nav_search).animate( {width: "100%"}, 750 ); })
-	.blur( function(e) { $(nav_search).animate( {width: "50%"}, 750 ); })
+	// Expand Search bar
+	nav_search = $("#nav-search");
+	nav_search
+	.focus( function(e){ nav_search.animate( {width: "100%"}, 750 ); })
+	.blur( function(e) { nav_search.animate( {width: "50%"}, 750 ); })
 	;
+	// Expand Search bar-
 	
-
 	// Show and Hide Login and Registeration Forms
 	var form_login_registeration = $("#form-login-registeration");
 	var form_login_registeration_links = $("#form-login-registeration-links");
@@ -20,6 +21,16 @@
 		form_login_registeration.children("div#"+hide).hide(700);
 		form_login_registeration.children("div#"+show).show(700);
 	});
+	// Show and Hide Login and Registeration Forms-
+
+	// Tooltip
+	options = {placement: "left"};
+	js_show_tooltip = $(".j-ttip");
+	js_show_tooltip.tooltip();
+	// Tooltip-
+
+
+
 
 })(jQuery);
 
